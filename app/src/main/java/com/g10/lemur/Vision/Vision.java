041d04576205a891fe1fa743780cb825913e00a1
André Caldegren <andweij@gmail.com>
2016-12-08 22:11:48 +0100

    fragment fix

--- app/src/main/java/com/g10/lemur/Vision/Vision.java
+++ app/src/main/java/com/g10/lemur/Vision/Vision.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -103,7 +104,7 @@ public class Vision extends AppCompatActivity implements NavigationView.OnNaviga
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragment = new ItemFragment();
-        fragmentTransaction.add(R.id.your_placeholder, fragment);
+        fragmentTransaction.replace(R.id.your_placeholder, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
